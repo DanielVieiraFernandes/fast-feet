c5c75a7c9df73f320185b4fab0f8746f64fc8b44
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-05-02 01:15:04 -0300

    feat(order): adiciona DTO para atualização de pedidos

---
+++ src/http/order/dto/update-order.dto.ts
@@ -0,0 +1,45 @@
+import { Transform } from 'class-transformer';
+import {
+  ArrayMaxSize,
+  ArrayNotEmpty,
+  IsArray,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  ValidateIf,
+  ValidationArguments,
+} from 'class-validator';
+
+export class UpdateOrderDto {
+  @IsUUID()
+  @IsOptional()
+  @Transform(({ value }) => (value === null ? undefined : value))
+  deliverymanId?: string;
+
+  @IsString()
+  @MaxLength(255, {
+    message: 'Detalhes devem ter no máximo 255 caracteres',
+  })
+  @Transform(({ value }) => (value === null ? undefined : value))
+  @IsOptional()
+  details?: string;
+
+  @IsArray()
+  @ArrayNotEmpty({ message: 'attachmentsIds não pode ser vazio' })
+  @ArrayMaxSize(1, {
+    message: 'deve ser anexada apenas uma foto ao entregar a a encomenda',
+  })
+  @IsUUID()
+  @Transform(({ value }) => (value === null ? undefined : value))
+  @IsOptional()
+  attachmentsIds?: string[];
+
+  @ValidateIf(o => !o.details && !o.recipientId)
+  validateAtLeastOneField(value: unknown, args: ValidationArguments) {
+    if (!value) {
+      throw new Error('At least one field must be provided');
+    }
+    return true;
+  }
+}
